cb0a4aa73174233f0179c793e6d7e8b1e7e5fcd5
metndurn <mtndrn123@gmail.com>
2024-12-15 21:31:31 +0300

    gereken sınıflar yazıldı ve icerikleri dolduruldu

---
+++ Entities/News.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities
+{
+	public class News : IEntity
+	{
+		public int Id { get; set; }
+		[Display(Name = "Başlık"), StringLength(150)]
+		public string Name { get; set; }
+		[Display(Name = "İçerik")]
+		public string Content { get; set; }
+		[Display(Name = "Resim"), StringLength(150)]
+		public string Image { get; set; }
+		[Display(Name = "Eklenme Tarihi"), ScaffoldColumn(false)]
+		public DateTime CreateDate { get; set; }
+
+	}
+}
---
+++ NetCoreWebDemo/Data/DataSeeding.cs
@@ -0,0 +1,34 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetCoreWebDemo.Data
+{
+	public class DataSeeding
+	{
+		public static void Seed(IApplicationBuilder app)
+		{
+			var scope = app.ApplicationServices.CreateScope();
+			var context = scope.ServiceProvider.GetService<DatabaseContext>();
+
+			context.Database.Migrate();
+
+			if (context.Database.GetPendingMigrations().Count() == 0)
+			{
+				if (context.Users.Count() == 0)
+				{
+					User user = new User()
+					{
+						Name = "Admin",
+						Password = "12345",
+						UserName = "Admin",
+						Email = "admin@NetCoreWebDemo.net",
+						IsActive = true,
+						CreateDate = DateTime.Now
+					};
+					context.Users.Add(user);
+				}
+				context.SaveChanges();
+			}
+		}
+	}
+}
