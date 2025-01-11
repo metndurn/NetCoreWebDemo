02fab965a8c3e1b02c9bded769ebdc0eb4ad8b46
metndurn <mtndrn123@gmail.com>
2025-01-11 19:09:40 +0300

    sliders ile resim işlemleri yapıldı

--- NetCoreWebDemo/Controllers/HomeController.cs
+++ NetCoreWebDemo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BL;
+using Microsoft.AspNetCore.Mvc;
 using NetCoreWebDemo.Models;
 using System.Diagnostics;
 
@@ -7,19 +8,28 @@ namespace NetCoreWebDemo.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
-
-		public HomeController(ILogger<HomeController> logger)
+		/*tek tek istenip kodlar kullanılacaktır anasayfa ıcın gereklıdır*/
+		CategoryManager categoryManager = new CategoryManager();
+		SliderManager sliderManager = new SliderManager();
+		NewsManager newsManager = new NewsManager();
+		PostManager postManager = new PostManager();
+		public HomeController(ILogger<HomeController> logger)//, CategoryManager categoryManager
 		{
 			_logger = logger;
+			//_categoryManager = categoryManager;
 		}
 
 		public IActionResult Index()
-		{
+		{/*ilgili alanların tek tek istenmesi gerekiyor bunu en basta verip daha sonra
+		  burada doldurmak ıcın managerlardan ısteyıp gosterıcez*/
 			var model = new HomePageViewModel()
 			{
-
+				Categories = categoryManager.GetAll(),
+				Sliders = sliderManager.GetAll(),
+				News = newsManager.GetAll(),
+				Posts = postManager.GetAll()
 			};
-			return View();
+			return View(model);
 		}
 
 		public IActionResult Privacy()
